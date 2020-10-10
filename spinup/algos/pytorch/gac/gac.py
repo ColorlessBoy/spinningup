0a036c3008cce7f54520deadfda871341a060ce5
penglingwei <501868024@qq.com>
2020-10-10 10:44:53 +0800

    Change: store more models.

--- spinup/algos/pytorch/gac/gac.py
+++ spinup/algos/pytorch/gac/gac.py
@@ -417,7 +417,7 @@ def gac(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
             # Save model
             if (epoch % save_freq == 0) or (epoch == epochs):
-                logger.save_state({'env': env}, None)
+                logger.save_state({'env': env}, t)
 
             # Test the performance of the deterministic version of the agent.
             test_agent()
