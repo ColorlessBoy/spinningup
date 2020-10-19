3fad18537d73610d7c3c6a6e0996d67382bb4e1c
penglingwei <501868024@qq.com>
2020-10-19 15:35:38 +0800

    Change: move ReplayBuffer to core.py.

--- spinup/algos/pytorch/gac/core.py
+++ spinup/algos/pytorch/gac/core.py
@@ -78,6 +78,54 @@ class MLPActorCritic(nn.Module):
                 a = self.pi(obs, noise=noise)
         return a.detach().cpu().numpy()[0]
 
+class ReplayBuffer:
+    """
+    A simple FIFO experience replay buffer for SAC agents.
+    """
+
+    def __init__(self, obs_dim, act_dim, size, obs_limit=5.0):
+        self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
+        self.obs2_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
+        self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.done_buf = np.zeros(size, dtype=np.float32)
+        self.ptr, self.size, self.max_size = 0, 0, size
+        # For state normalization.
+        self.total_num = 0
+        self.obs_limit = 5.0
+        self.obs_mean = np.zeros(obs_dim, dtype=np.float32)
+        self.obs_square_mean = np.zeros(obs_dim, dtype=np.float32)
+        self.obs_std = np.zeros(obs_dim, dtype=np.float32)
+        self.obs_normalization = True
+
+    def store(self, obs, act, rew, next_obs, done):
+        self.obs_buf[self.ptr] = obs
+        self.obs2_buf[self.ptr] = next_obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.ptr = (self.ptr+1) % self.max_size
+        self.size = min(self.size+1, self.max_size)
+
+        if self.obs_normalization:
+            self.total_num += 1
+            self.obs_mean = self.obs_mean / self.total_num * (self.total_num - 1) + obs / self.total_num
+            self.obs_square_mean = self.obs_square_mean / self.total_num * (self.total_num - 1) + obs**2 / self.total_num
+            self.obs_std = np.sqrt(self.obs_square_mean - self.obs_mean ** 2 + 1e-8)
+
+    def sample_batch(self, batch_size=32):
+        idxs = np.random.randint(0, self.size, size=batch_size)
+        batch = dict(obs=self.obs_encoder(self.obs_buf[idxs]),
+                     obs2=self.obs_encoder(self.obs2_buf[idxs]),
+                     act=self.act_buf[idxs],
+                     rew=self.rew_buf[idxs],
+                     done=self.done_buf[idxs])
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
+    
+    def obs_encoder(self, o):
+        return ((np.array(o) - self.obs_mean)/(self.obs_std + 1e-8)).clip(-self.obs_limit, self.obs_limit)
+
+
 # Maximum Mean Discrepancy
 # geomloss: https://github.com/jeanfeydy/geomloss
 
