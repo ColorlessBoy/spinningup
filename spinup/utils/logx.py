8b397a39a7ad2f8cd677a68bdaa395caa9522f3f
colorlessboy <501868024@qq.com>
2020-12-09 21:57:34 +0800

    stupid mpi_statistics_scalar

--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -31,6 +31,30 @@ color2num = dict(
 def proc_id():
     return 0
 
+def mpi_statistics_scalar(x, with_min_and_max=False):
+    """
+    Get mean/std and optional min/max of scalar x across MPI processes.
+
+    Args:
+        x: An array containing samples of the scalar to produce statistics
+            for.
+
+        with_min_and_max (bool): If true, return min and max of x in 
+            addition to mean and std.
+    """
+    x = np.array(x, dtype=np.float32)
+    global_sum, global_n = [np.sum(x), len(x)]
+    mean = global_sum / global_n
+
+    global_sum_sq = np.sum((x - mean)**2)
+    std = np.sqrt(global_sum_sq / global_n)  # compute global std
+
+    if with_min_and_max:
+        global_min = np.min(x)
+        global_max = np.max(x)
+        return mean, std, global_min, global_max
+    return mean, std
+
 def colorize(string, color, bold=False, highlight=False):
     """
     Colorize a string.
